64c66cfeee816a462c001b5c087a5ffb3eea749c
Matheus Maul <matheusmaul64@gmail.com>
2026-07-15 18:45:38 -0300

    Criado model sitesetup

---
+++ djangoapp/site_setup/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class MenuLink(models.Model):
+    class Meta:
+        verbose_name = 'Menu link'
+        verbose_name_plural = 'Menu links'
+
+    text = models.CharField(max_length=50)
+    url_or_path = models.CharField(max_length=50)
+    new_tab = models.BooleanField(default= False)
+
+    def __str__(self):
+        return self.text
+    
+class SiteSetup(models.Model):
+    class Meta:
+        verbose_name = 'Setup'
+        verbose_name_plural = 'Setups'
+
+    text = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    show_header = models.BooleanField(default= True)
+    show_search = models.BooleanField(default= True)
+    show_menu = models.BooleanField(default= True)
+    show_description = models.BooleanField(default= True)
+    show_pagination = models.BooleanField(default= True)
+    show_footer = models.BooleanField(default= True)
